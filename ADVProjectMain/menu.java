d8d5cf156ac93a98f4245922a24f07bc9511d9a7
Filip Rudnicki <100528614@ea.edin.sch.uk>
2021-11-16 12:56:35 +0000

    Menu change :D

--- ADVProjectMain/menu.java
+++ ADVProjectMain/menu.java
@@ -3,11 +3,11 @@ package ADVProjectMain;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 public class menu extends JFrame{
     
     menu() {
-    
     int dif = 0;
     JFrame f = new JFrame();
 
@@ -21,18 +21,21 @@ public class menu extends JFrame{
     b1.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e) {
                 int dif = 8;
+                System.out.println(dif);
         }    
         });
 
     b2.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e) {
                 int dif = 14;
+                System.out.println(dif);
         }    
         });
 
     b3.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 int dif = 20;
+                System.out.println(dif);
         }    
         });
 
@@ -55,9 +58,26 @@ public class menu extends JFrame{
     f.setVisible(true);
     //f.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 
-    return actionPerformed(dif);
+    int rows = dif;
+    int columns = (rows + 2);
+    Random rand = new Random();
+        
+        //Randomly generate mines in set size
+    
+        int m = 0;
+        int [][] mine = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                double r = rand.nextDouble();
+                if (r < rows/40) m = 1;
+                else m = 0;
+            
+                mine[i][j] = m; 
+            }
+        }
 }
     public static void main(String[] args) {
         new menu();
+        
     }    
 }
