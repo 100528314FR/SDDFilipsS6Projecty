5188b89348160d950b45d9a636baa8a7440c960c
Filip Rudnicki <100528614@ea.edin.sch.uk>
2021-11-16 13:00:43 +0000

    ;l

--- ADVProjectMain/menu.java
+++ ADVProjectMain/menu.java
@@ -1,64 +1,32 @@
 package ADVProjectMain;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 import java.util.Random;
 
-public class menu extends JFrame{
+public class  menu extends JFrame{
     
     menu() {
-    int dif = 0;
+    int dif = 1;
     JFrame f = new JFrame();
+    Object[] options = {"Easy", "Medium", "Hard"};
+    dif = JOptionPane.showOptionDialog(null, "Select a difficulty", "Difficulty",
+          JOptionPane.YES_NO_CANCEL_OPTION,
+          JOptionPane.QUESTION_MESSAGE,
+          null,
+          options, 
+          options[1] );
 
-    //JDialog d = new JDialog(f, "Menu", true);
-    
-    JButton b1 = new JButton("Easy");
-    JButton b2 = new JButton("Medium");
-    JButton b3 = new JButton("Hard");
-    JButton b = new JButton("EXIT");
 
-    b1.addActionListener(new ActionListener(){
-        public void actionPerformed(ActionEvent e) {
-                int dif = 8;
-                System.out.println(dif);
-        }    
-        });
-
-    b2.addActionListener(new ActionListener(){
-        public void actionPerformed(ActionEvent e) {
-                int dif = 14;
-                System.out.println(dif);
-        }    
-        });
-
-    b3.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                int dif = 20;
-                System.out.println(dif);
-        }    
-        });
-
-    b.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-        }    
-        });
-
-    //ImageIcon i = new ImageIcon("/Users/pupil19/Desktop/mug.jpg");
-    JLabel l = new JLabel("Select a difficulty:");
-    f.add(l);
-    f.add(b1);
-    f.add(b2);
-    f.add(b3);
-    f.add(b);
-
-    f.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 200));
-    f.setSize(500,500);
-    f.setVisible(true);
     //f.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 
-    int rows = dif;
+    int rows;
+    if (dif == 0) {
+        rows = 8;
+    }
+    else if (dif == 1) {
+        rows = 14;
+    }
+    else rows = 20;
     int columns = (rows + 2);
     Random rand = new Random();
         
@@ -69,11 +37,17 @@ public class menu extends JFrame{
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 double r = rand.nextDouble();
-                if (r < rows/40) m = 1;
+                if (r < rows/10) m = 1;
                 else m = 0;
             
                 mine[i][j] = m; 
             }
+        }
+        for (int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                System.out.print(mine[i][j]);
+            }
+        System.out.println();
         }
 }
     public static void main(String[] args) {
