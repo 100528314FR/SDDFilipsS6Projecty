039c0110e00a5c2fe011627a57ac42d5af8dc730
Filip Rudnicki <100528614@ea.edin.sch.uk>
2021-11-23 14:11:03 +0000

    :D

--- ADVProjectMain/grid.java
+++ ADVProjectMain/grid.java
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-package ADVProjectMain;
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
-=======
-import javax.swing.*;
-import java.awt.*;
->>>>>>> 04801288c169da07a39a0813f19ccae5b5c8b577
 
 public class grid {
     
@@ -17,54 +10,45 @@ public class grid {
     JFrame f = new JFrame();
     JPanel p = new JPanel();
     JButton[][]buttons = new JButton[rows][columns];
-<<<<<<< HEAD
     
-    public grid() {
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-=======
+    int[][] count = new int[rows][columns];
+
 
     public grid() {
         f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         f.setVisible(true);
->>>>>>> 04801288c169da07a39a0813f19ccae5b5c8b577
         f.setSize(700, 600);
         p.setLayout(new GridLayout(rows, columns));
-        
         addButtons();
         f.add(p);
-<<<<<<< HEAD
         f.setVisible(true);
     }
 
-    public class mines {
-        
-        int[][] mines = new int[rows][columns];
-
-        public int[][] mineGen() {
+public class mines {
+    public int[][] mineGen() {
+            int[][] minef = new int[rows][columns];
             int m;
             double r = 0;
             Random rand = new Random();
-    
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
                     r = rand.nextDouble();
                     if (r <= 0.2) m = 9;
                     else m = 0;
-                    mines[i][j] = m;
+                    minef[i][j] = m;
                 }
             }
-            return mines;
+            return minef;
         }
     
         public int[][] counts() {
-            int[][] count = new int[3][3];
-            int num = 0;
+            int num = 1;
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
-                        if (mines[i][j] != 9) {
+                        if (minef[i][j] != 9) {
                             for (int a = i - 1; a < i + 1; a++) {
                                 for (int b = j + 1; b < j - 1; b++) {
-                                    if (mines[a][b] == 9) {
+                                    if (minef[a][b] == 9) {
                                         num = num+1;
                                 }   
                             }
@@ -75,51 +59,24 @@ public class grid {
             }
             return count;
         }
-    }
+}    
     
-    
-
     public void addButtons() {
-        new mines();
-        new counts();
-        /*int m;
-        double r = 0;
-        Random rand = new Random(); */
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                //r = rand.nextDouble();
-                //if (r <= 0.2) m = 9;
-                //else m = 0;
-                //mines[i][j] = m; 
                 
-                if (mines[i][j] == 9) {
+                if (minef[i][j] == 9) {
                     buttons[i][j] = new JButton(9+" ");
                 } else {
                     buttons[i][j] = new JButton(count[i][j]+" ");
                 }
                 
-=======
-    }
-    public void addButtons() {
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                buttons[i][j] = new JButton(i+1+", "+(j+1));
->>>>>>> 04801288c169da07a39a0813f19ccae5b5c8b577
                 p.add(buttons[i][j]);
             }
-        }
+        }   
     }
-<<<<<<< HEAD
+}
 
-    public static void main (String[] args) {
-        new grid();
-    }
-}
-=======
-    public static void main (String[] args) {
-        new grid();
-    }
-}
->>>>>>> 04801288c169da07a39a0813f19ccae5b5c8b577
+
+
+
--- ADVProjectMain/menu.java
+++ ADVProjectMain/menu.java
@@ -1,87 +1,35 @@
-
 import javax.swing.*;
 
-<<<<<<< HEAD
-public class menu extends JFrame{
-
-    static int rows;
-    static int columns;
-    static int[][] mine;
-=======
 public class  menu extends JFrame{
     static int rows;
     static int columns;
->>>>>>> 04801288c169da07a39a0813f19ccae5b5c8b577
     
     public menu() {
     int dif = 1;
     JFrame f = new JFrame();
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Object[] options = {"Hard", "Medium", "Easy"};
+    Object[] options = {"Easy", "Medium", "Hard"};
     dif = JOptionPane.showOptionDialog(null, "Select a difficulty", "Difficulty",
           JOptionPane.YES_NO_CANCEL_OPTION,
           JOptionPane.QUESTION_MESSAGE,
           null,
           options, 
           options[1] );
-<<<<<<< HEAD
-    if (dif == 2) {
-=======
-
-
-    //f.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 
     if (dif == 0) {
->>>>>>> 04801288c169da07a39a0813f19ccae5b5c8b577
         rows = 8;
     }
     else if (dif == 1) {
         rows = 14;
     }
     else rows = 20;
-<<<<<<< HEAD
+
     columns = (rows + 4);
     }
     public static void main(String[] args) {
         new menu();
         new grid();
-=======
+    }
+}
 
-    columns = (rows + 2);
-    Random rand = new Random();
-        
-        //Randomly generate mines in set size
     
-
-        int [][] mine = new int[rows][columns];
-        int m;
-        double r = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                r = rand.nextDouble();
-                if (r <= 0.3) m = 1;
-                else m = 0;
-                mine[i][j] = m; 
-            
-            }
-            
-        }
-
-        for (int[] rows: mine) {
-            if(rows != null) {
-                for( int cols: rows) {
-                    System.out.print(cols + " ");
-                }
-            }
-            System.out.println();
-        }
-
-
-}
-    public static void main(String[] args) {
-        new menu();
-        new grid();
-        
->>>>>>> 04801288c169da07a39a0813f19ccae5b5c8b577
-    }    
-}
