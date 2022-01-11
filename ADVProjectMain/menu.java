c01f38a36f575be33dab8204aa78a72191681813
Filip Rudnicki <100528614@ea.edin.sch.uk>
2022-01-11 22:23:52 +0000

    Can now add name, figure out timer now

--- ADVProjectMain/menu.java
+++ ADVProjectMain/menu.java
@@ -1,29 +1,46 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 
 public class  menu extends JFrame{
     static int rows = 1;
     static int columns;
-    
+    static String name;
+
     public menu() {
         new diff();
         if (rows >= 8) {
             new name();
         }
     }
-    public class name {
+    public class name extends Frame {
         public name() {
-            String name = "";
+            
             JFrame fn = new JFrame();
-            JLabel l = new JLabel("Enter your name");
-            JTextField tf = new JTextField();
-            tf.setBounds();
+            JLabel l = new JLabel("Enter your name:");
+            JButton b = new JButton("Ok");
+            JTextArea tf = new JTextArea();
+            fn.setLayout(null);
+            l.setBounds(20, 10, 200, 40);
+            l.setFont(new Font("Verdana", Font.BOLD, 15));
+            tf.setBounds(20, 45, 150, 20);
+            b.setBounds(125, 75, 50, 25);
+            b.addActionListener(new ActionListener(){  
+                public void actionPerformed(ActionEvent e){  
+                            name = tf.getText(); 
+                            System.out.println(name);
+                            new grid();
+                            fn.dispose();
+                        }  
+                    });  
             fn.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            fn.add(l);
-            fn.add(tf);
-            fn.setSize(150,100);
-            fn.setVisible(true);
             fn.setLocationRelativeTo(null);
+            fn.add(tf);
+            fn.add(l);
+            fn.add(b);
+            fn.setSize(200,150);
+            fn.setVisible(true);
         }
     }
     public class diff {
@@ -57,9 +74,6 @@ public class  menu extends JFrame{
 
 public static void main(String[] args) {
         new menu();
-        if (rows >= 8) {
-        new grid();
-        }
     }
 }
 
